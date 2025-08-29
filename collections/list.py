ef341597fc2b14a079052442c51a60ce707376a4
Mohammad Ali <mscsapan@gmail.com>
2025-08-29 22:17:22 +0600

    🌱 List file added and learned till list comprehension

--- collections/list.py
+++ collections/list.py
@@ -1,1 +1,17 @@
-fruits_name = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+fruits_name = ["apple", "banana", "cherry"]
+
+extendable_list = ["orange", "kiwi", "melon", "mango"]
+
+
+# for i in dir(fruits_name):
+#     print(i)
+
+# fruits_name.clear()
+# fruits_name.reverse()
+# fruits_name.remove('apple')
+# fruits_name.pop(0)
+# fruits_name.append('grapes')
+# fruits_name.extend(extendable_list)
+del fruits_name
+
+print(fruits_name)
