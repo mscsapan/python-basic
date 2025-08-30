40f5bb872913fc7a9dc58676fa58690b38b5de28
Mohammad Ali <mscsapan@gmail.com>
2025-08-30 21:27:45 +0600

    ✅ More list function or approaches added

--- collections/list.py
+++ collections/list.py
@@ -1,6 +1,12 @@
-fruits_name = ["apple", "banana", "cherry"]
+fruits_name = ["Apple", "banana", "cherry"]
+
+# fruits2 = fruits_name.copy() # or fruits2 = fruits_name[:] (using : slice operator)
 
 extendable_list = ["orange", "kiwi", "melon", "mango"]
+
+numbers = [1,2,3,5,9,0,8]
+
+# new_list = []
 
 
 # for i in dir(fruits_name):
@@ -12,6 +18,36 @@ extendable_list = ["orange", "kiwi", "melon", "mango"]
 # fruits_name.pop(0)
 # fruits_name.append('grapes')
 # fruits_name.extend(extendable_list)
-del fruits_name
+# del fruits_name
 
-print(fruits_name)
+# for i in range(len(fruits_name)):
+#     print(f'fruits name {fruits_name[i]}')
+    
+# for i in fruits_name:
+#     print(f'fruits name {i}')
+
+# for index, name in enumerate(fruits_name):
+#     print(f'fruits name {name}')
+
+# for name in fruits_name:
+#     print(f'fruits name {name}')
+
+
+
+# for i in numbers:
+#     if i > 0:
+#         square = i ** 2
+#         new_list.append(square)
+
+
+# new_list = [x ** 2 for x in numbers]
+    
+# print(new_list)
+
+
+    
+# list = [print(i + 1) for i in range(10)]
+
+
+# fruits_name.sort(reverse = True,key=str.lower)
+# print(fruits2)
