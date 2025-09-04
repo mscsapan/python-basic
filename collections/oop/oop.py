61d7cce122335d5921f50c963e0b4f93c562a25b
Mohammad Ali <mscsapan@gmail.com>
2025-09-04 22:27:00 +0600

    📝 Class default function tested

--- collections/oop/oop.py
+++ collections/oop/oop.py
@@ -1,10 +1,22 @@
 class Animal:
+    
+    def __init__(self,name,age):
+        self.name = name,
+        self.age = age,
+        print(f'name: {self.name}, age : {self.age}')
+        
+    
+    # def __str__(self):
+    #      print(f'name: {self.name}, age : {self.age}')
+    
     def animal_name(self):
         return 'Hello function'
         
 
 
-animal = Animal()
+animal = Animal('Ali',26)
 
-name = animal.animal_name()
-print(animal.animal_name())
+# name = animal.animal_name()
+# print(animal.animal_name())
+
+animal.name
