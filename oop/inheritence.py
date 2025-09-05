90e492077a21b376b1dbf79f5043227990360ca1
Mohammad Ali <mscsapan@gmail.com>
2025-09-05 10:09:43 +0600

    ✅ Inherience file added

---
+++ oop/inheritence.py
@@ -0,0 +1,29 @@
+# class Person:
+#     def __init__(self,name,age,address:str):
+#         self.name = name
+#         self.age = age
+#         self.address = address
+
+
+
+# class Student(Person):
+#     pass
+
+# p1 = Student('Ali',27,'Dhaka')
+
+
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+
+class Student(Person):
+    def __init__(self,name,age):
+        # Person.__init__(self,name,age)
+        super().__init__(name,age)
+
+p1 = Student('Ali',27)
+
+print(p1.name)
