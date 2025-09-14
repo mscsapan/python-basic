5f5c3921a64fae67c308c9e7517dd89f00143293
Mohammad Ali <mscsapan@gmail.com>
2025-09-14 21:25:55 +0600

    🧱 Oop inheritence code modified

--- oop/inheritence.py
+++ oop/inheritence.py
@@ -5,25 +5,26 @@
 #         self.address = address
 
 
-
 # class Student(Person):
 #     pass
 
 # p1 = Student('Ali',27,'Dhaka')
 
 
-
 class Person:
-    def __init__(self,name,age):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
 
 
 class Student(Person):
-    def __init__(self,name,age):
+    def __init__(self, name, age):
         # Person.__init__(self,name,age)
-        super().__init__(name,age)
+        super().__init__(name, age)
 
-p1 = Student('Ali',27)
+
+p1 = Student('Ali', 27)
 
 print(p1.name)
+
+print('hello bangladesh')
--- oop/oop.py
+++ oop/oop.py
@@ -1,22 +1,19 @@
 class Animal:
-    
-    def __init__(self,name,age):
+
+    def __init__(self, name, age):
         self.name = name
         self.age = age
         print(f'name: {self.name}, age : {self.age}')
-        
-    
+
     # def __str__(self):
     #      print(f'name: {self.name}, age : {self.age}')
-    
+
     def animal_name(self):
-        return 'Hello function '+self.name
-        
+        return 'Hello function ' + self.name
 
 
-animal = Animal('Ali',27)
+animal = Animal('Ali', 27)
 
 print(animal.animal_name())
-
 
 # name = animal.animal_name()
