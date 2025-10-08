3668659461255facb305aa14076317de3d448171
Mohammad Ali <mscsapan@gmail.com>
2025-10-08 20:51:31 +0600

    🚩 Asterisk file edited

--- asterisk.py
+++ asterisk.py
@@ -1,6 +1,27 @@
-number = [9] * 8
+# number = 'Ali' ' '* 8
 
-print(number)
-print(number[0])
+# print(type(number))
+
+# def numers(**kwargs):
+#     return kwargs
+
+# result = numers(name='Ali',age=26,address='Dhaka, Bangladesh')
+
+# for key, value in result.items():
+#     print(f'{key} - {value}')
 
 
+my_list = ['a','b','c','d','e','f','g']
+
+my_tuple = tuple(my_list)
+
+
+# for key, value in enumerate(my_list):
+#     print(f'{key} - {value} - {type(value)}')
+
+# for key, value in enumerate(my_tuple):
+#     print(f'{key} - {value} - {type(value)}')
+
+a, b, c , *d= my_tuple
+
+print(type(d))
