b60114b2d5ca9b27824eeda44ca017f7e0b5f445
Mohammad Ali <mscsapan@gmail.com>
2026-02-02 18:55:18 +0600

    🚧 Working with python tuple method

--- collections/practice.py
+++ collections/practice.py
@@ -3,7 +3,7 @@ tuple_numbers = (1,2,3,4,5,5,5,6,7,8,9,10)
 dict_numbers = {'name':'Ali','age':26}
 set_numbers = {1, 2, 3, 4, 5, 6, 7}
 
-print(tuple_numbers.count(5))
+print(tuple_numbers.index(4))
 num = [*list_numbers,tuple_numbers]
 
 print(num)
