5ec8a84dceeb0aa8a1a209c12e73524cf3e48426
Mohammad Ali <mscsapan@gmail.com>
2026-04-14 08:24:48 +0600

    🌱 Data type file added

---
+++ practise/data_type.py
@@ -0,0 +1,25 @@
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+
+name = 'Ali'
+age = 28
+address = 'Dhaka, Bangladesh'
+cgpa = 3.20
+complex_number = 2j
+
+list_type = ["apple", "banana", "cherry"]
+tuple_type = ("apple", "banana", "cherry")
+range_type = range(6)
+dict_type = {"name": "John", "age": 36}
+set_type = {"apple", "banana", "cherry"}
+frozen_type = frozenset({"apple", "banana", "cherry"})
+none_type = None
+is_married = True
+
+print(type(none_type))
