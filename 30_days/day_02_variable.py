592e54f891dfa55de7ad80aec1e62722f2f975f8
Mohammad Ali <mscsapan@gmail.com>
2026-05-03 21:13:36 +0600

    📝 Variable file added

---
+++ 30_days/day_02_variable.py
@@ -0,0 +1,16 @@
+first_name = 'Mohammad'
+last_name = 'Ali'
+country = 'Bangladesh'
+city = 'Dhaka'
+age = 25
+is_married = True
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python'],
+hobby = ('travelling','learning','coding')
+person_info = {
+    'firstname': 'Mohammad',
+    'lastname': 'Ali',
+    'country': 'Bangladesh',
+    'city': 'Dhaka',
+    'age': 26
+}
+
